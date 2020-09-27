aa6ba008a0d8e99fe07a887d21e1350cc45a25d9
k-furumichi <k-furumichi@sakura.ad.jp>
2020-09-28 06:39:49 +0900

    修正

--- irkit_flask/app/app.py
+++ irkit_flask/app/app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
+import post_irkit
+from const import *
 
 app = Flask(__name__)
+irkit = post_irkit.Post_irkit
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/light")
+def light():
+    irkit.req_post(LIGHT_SIG)
+
+@app.route("/ac-on")
+def ac-on():
+    irkit.req_post(AC_ON_SIG)
+
+@app.route("/ac-off")
+def ac-off():
+    irkit.req_post(AC_OFF_SIG)
 
 if __name__ == '__main__':
     app.run()
---
+++ irkit_flask/app/post_irkit.py
@@ -0,0 +1,16 @@
+import requests
+import json
+from const import *
+
+class Post_irkit:
+    def __init__(self):
+        pass
+
+    def req_post(self, signal):
+        msg = json.dumps(signal)
+        params = {
+            "clientkey":CLIKEY,
+            "deviceid":DEVID,
+            "message":msg
+        }
+        req = requests.post(URL, params=params)
