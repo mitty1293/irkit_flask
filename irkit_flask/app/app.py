5888d4a356d19e9c2d9865ef7ef24b08a15e4c93
mitty1293 <k.fmichi@gmail.com>
2020-09-29 23:48:29 +0900

    class宣言の修正

--- irkit_flask/app/app.py
+++ irkit_flask/app/app.py
@@ -3,7 +3,7 @@ import post_irkit
 from const import *
 
 app = Flask(__name__)
-irkit = post_irkit.Post_irkit
+irkit = post_irkit.Post_irkit()
 
 @app.route("/")
 def index():
@@ -14,19 +14,19 @@ def light():
     irkit.req_post(LIGHT_SIG)
 
 @app.route("/ac-on")
-def ac-on():
+def ac_on():
     irkit.req_post(AC_ON_SIG)
 
 @app.route("/ac-off")
-def ac-off():
+def ac_off():
     irkit.req_post(AC_OFF_SIG)
 
 @app.route("/heater-on")
-def heater-on():
+def heater_on():
     irkit.req_post(HEATER_ON_SIG)
 
 @app.route("/heater-off")
-def heater-off():
+def heater_off():
     irkit.req_post(HEATER_OFF_SIG)
 
 if __name__ == '__main__':
