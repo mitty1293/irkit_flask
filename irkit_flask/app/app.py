fcf559ccd207814306261b9f8f068c22313e1621
mitty1293 <k.fmichi@gmail.com>
2020-09-30 00:52:33 +0900

    req_successの追加

--- irkit_flask/app/app.py
+++ irkit_flask/app/app.py
@@ -12,22 +12,27 @@ def index():
 @app.route("/light")
 def light():
     irkit.req_post(LIGHT_SIG)
+    return render_template("req_success.html")
 
 @app.route("/ac-on")
 def ac_on():
     irkit.req_post(AC_ON_SIG)
+    return render_template("req_success.html")
 
 @app.route("/ac-off")
 def ac_off():
     irkit.req_post(AC_OFF_SIG)
+    return render_template("req_success.html")
 
 @app.route("/heater-on")
 def heater_on():
     irkit.req_post(HEATER_ON_SIG)
+    return render_template("req_success.html")
 
 @app.route("/heater-off")
 def heater_off():
     irkit.req_post(HEATER_OFF_SIG)
+    return render_template("req_success.html")
 
 if __name__ == '__main__':
     app.run()
