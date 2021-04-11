a6937f0c0b6a3945bd522bce13dd27c44d35b74f
mitty1293 <k.fmichi@gmail.com>
2021-04-11 14:09:14 +0900

    fix

--- irkit_flask/app/post_irkit.py
+++ irkit_flask/app/post_irkit.py
@@ -9,6 +9,7 @@ class Post_irkit:
     def req_post(self, signal):
         msg = json.dumps(signal)
         params = {
+            # IRKit Internet HTTP APIを用いる場合は以下を使う
             #"clientkey":CLIKEY,
             #"deviceid":DEVID,
             "message":msg
